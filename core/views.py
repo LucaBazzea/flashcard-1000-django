585fe78c002398054499570e981af363b684619e
LucaBazzea <luca.bazzea@gmail.com>
2022-05-05 20:38:25 +0200

    index.html now displays data from views

--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
 
+def get_words():
+    ita_word = "Ciao"
+    eng_word = "Hello"
+
+    word = {
+        "ita_word":ita_word,
+        "eng_word":eng_word
+    }
+
+    return word
+
+
 def index(request):
     context = {
-        "ita_word":"Ciao",
-        "eng_word":"Hello"
+        "word":get_words()
     }
+
     return render(request, "index.html", context)
