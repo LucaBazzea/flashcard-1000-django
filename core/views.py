4049339c7b4180a17d170474e9428e57d8848098
LucaBazzea <luca.bazzea@gmail.com>
2022-05-12 21:35:21 +0200

    Looping through words from json file

--- core/views.py
+++ core/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-
+import json
+from random import randint
 
 def get_words():
-    ita_word = "Ciao"
-    eng_word = "Hello"
+    
+    with open("core/data/nouns.json", "r") as file:
+        data = json.load(file)
+
+    word_index = randint(0, 100)
 
     word = {
-        "ita_word":ita_word,
-        "eng_word":eng_word
+        "ita_word":data["nouns"][word_index]["ita"],
+        "eng_word":data["nouns"][word_index]["eng"]
     }
-
+    
     return word
 
 
