8f0a3b311b290dbebf1113a3eba77db40c513700
LucaBazzea <luca.bazzea@gmail.com>
2022-05-16 15:20:36 +0200

    Looping through words from a new json file

--- core/views.py
+++ core/views.py
@@ -4,14 +4,14 @@ from random import randint
 
 def get_words():
     
-    with open("core/data/nouns.json", "r") as file:
+    with open("core/data/ita_words.json", "r") as file:
         data = json.load(file)
 
     word_index = randint(0, 100)
 
     word = {
-        "ita_word":data["nouns"][word_index]["ita"],
-        "eng_word":data["nouns"][word_index]["eng"]
+        "ita_word":data["words"][word_index]["ita"],
+        "eng_word":data["words"][word_index]["eng"]
     }
     
     return word
