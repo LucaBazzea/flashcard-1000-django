2de5fd21d88d670171cba0974902483760840136
LucaBazzea <luca.bazzea@gmail.com>
2022-05-19 19:33:46 +0200

    Installed django-unicorn

--- core/urls.py
+++ core/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index")
+    path("", views.get_index, name="get_index"),
+    path("unicorn/", include("django_unicorn.urls")),
+    path("next-word", views.get_words, name="get_words")
 ]
